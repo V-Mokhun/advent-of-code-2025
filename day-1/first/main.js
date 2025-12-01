db333dcbd1dca95036ed25b0a62dbdaf00e96054
v-mokhun <v.mokhun@gmail.com>
2025-12-01 22:20:19 +0100

    init and add day-1

---
+++ day-1/first/main.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+let input = fs.readFileSync("../input.txt", "utf-8");
+input = input.split("\n");
+
+let zerosCount = 0;
+let startPoint = 50;
+
+for (let i = 0; i < input.length; i++) {
+  const line = input[i];
+  let [direction, ...steps] = line.split("");
+  steps = Number(steps.join(""));
+  if (direction === "L") {
+    startPoint -= steps;
+
+    while (startPoint < 0) {
+      startPoint += 100;
+    }
+    if (startPoint === 0) {
+      zerosCount++;
+    }
+  } else {
+    startPoint += steps;
+
+    while (startPoint >= 100) {
+      startPoint -= 100;
+    }
+
+    if (startPoint === 0) {
+      zerosCount++;
+    }
+  }
+}
+
+console.log("Result: ", zerosCount);
